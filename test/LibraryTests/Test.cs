3f0b19b4aed5ef8054218c81a694135ea6986bb4
Guss310 <105060468+Guss310@users.noreply.github.com>
2022-12-21 15:25:51 -0300

    Add files via upload

---
+++ test/LibraryTests/Test.cs
@@ -0,0 +1,28 @@
+using AdapterExample;
+using NUnit.Framework;
+
+using NecessaryClass;
+
+
+namespace Tests
+{
+    /// <summary>
+    /// Clase de tests
+    /// </summary>
+    public class SmartDevicesTests
+    {
+        /// <summary>
+        /// Se testea que un Plug es adaptado a ISmartDevice
+        /// </summary>
+        /// 
+        [TestFixture]
+        public void AdapterTest()
+        {
+            Plug plug = new Plug("1");
+            ISmartDevice SmartPlug = new driver(plug);
+            SmartPlug.Off();
+            SmartPlug.On();
+            Assert.AreEqual(SmartPlug.GetStatus(), "on");
+        }
+    }
+}
